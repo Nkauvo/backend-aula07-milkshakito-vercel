f598d9c2225021b7080ffdb77ec4cd6dfabcd4e3
Gabriel <gabriel.a.siqueira11@aluno.senai.br>
2026-03-24 07:50:27 -0300

    Add files via upload

--- routes/categorias.js
+++ routes/categorias.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../data/database');
+const supabase = require('../data/supabase');
 
-router.get('/', (req, res) => {
-    res.json(db.categorias);
+router.get('/', async (req, res, next) => {
+    try{
+        const { data, error } = await supabase
+        .from('categorias')
+        .select('*')
+        .order('id', {ascending: true})
+
+        if (error){
+        throw error;
+    }
+    res.json(data);
+    }catch (err) {
+        next(err)
+    }
 });
 
-router.post('/', (req, res) => {
-    const novaCategoria = {
-        id: db.categorias.length + 1,
-        nome: req.body.nome
-    };
-    db.categorias.push(novaCategoria);
-    res.status(201).json(novaCategoria);
+router.post('/', async (req, res, next) => {
+    try{
+        const { data, error } = await supabase
+        .from('categorias')
+        .insert([{nome: req.body}])
+        .select();
+
+        if (error) throw error;
+    
+    res.status(201).json(data[0]);
+    }catch (err) {
+        next(err)
+    }
 });
 
 module.exports = router;
-
-// URL: http://localhost:3000/api/categorias
