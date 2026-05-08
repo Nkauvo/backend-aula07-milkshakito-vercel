375679714290ba2232cd0e07e7f6ad0e83190954
Gabriel <gabrielveneruci@gmail.com>
2026-05-08 11:01:32 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 
-// Importando nossos Middlewares customizados
+// Importando nossos Middlewares customizados␊
 const logger = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
-
 
 // Middlewares Globais
 app.use(cors());
@@ -17,7 +16,7 @@ app.use(logger);
 
 // Rota de teste
 app.get('/', (req, res) => {
-    res.json({ mensagem: '🍦 Bem-vindo a API do MIlkshakito!'});
+  res.json({ mensagem: '🍦 Bem-vindo a API do MIlkshakito!' });
 });
 
 // Importando rotas
@@ -31,35 +30,36 @@ app.use('/api/produtos', rotasProdutos);
 app.use('/api/pedidos', rotasPedidos);
 
 // Tratamento de Rotas não encontradas (Erro 404)
-app.use((req, res, next) => {
-    res.status(404).json({ mensagem: "Rota não encontrada na API do MIlk shakito." });
+app.use((req, res) => {
+  res.status(404).json({ mensagem: 'Rota não encontrada na API do MIlk shakito.' });
 });
 
 // MIDDLEWARE DE ERROS GLOBAL (Sempre no final!)
 app.use(errorHandler);
 
-// Iniciar o servidor
-const PORTA = process.env.PORT || 3000;
+if (require.main === module) {
+  const PORTA = process.env.PORT || 3000;
 
-app.listen(PORTA, () => {
-       console.log('');
+  app.listen(PORTA, () => {
+    console.log('');
     console.log('🚀 ================================');
-    console.log(`🚀 Servidor rodando!`);
+    console.log('🚀 Servidor rodando!');
     console.log(`🚀 Porta local: ${PORTA}`);
     console.log('🚀 ================================');
     console.log('');
     console.log('📋 Rotas disponíveis:');
-    console.log(`   GET    /api/categorias`);
-    console.log(`   POST   /api/categorias`);
-    console.log(`   GET   /api/produtos`);
-    console.log(`   GET    /api/produtos/:id`);
-    console.log(`   POST   /api/produtos`);
-    console.log(`   PUT    /api/produtos/:id`);
-    console.log(`   DELETE /api/produtos/:id`);
+    console.log('   GET    /api/categorias');
+    console.log('   POST   /api/categorias');
+    console.log('   GET    /api/produtos');
+    console.log('   GET    /api/produtos/:id');
+    console.log('   POST   /api/produtos');
+    console.log('   PUT    /api/produtos/:id');
+    console.log('   DELETE /api/produtos/:id');
     console.log('');
     console.log('💣 Rota de teste de erro:');
-    console.log(`   GET    /api/produtos/erro-teste`);
-    console.log('');;
-});
+    console.log('   GET    /api/produtos/erro-teste');
+    console.log('');
+  });
+}
 
 module.exports = app;
